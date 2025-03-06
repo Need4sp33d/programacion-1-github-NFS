9a4ffebd0208a3e0d7b91d8ddae21670d06b1dd4
JoJo <alguienreal100@gmail.com>
2025-03-05 23:18:54 -0500

    version 0.2.0

--- Archivos github/programacion 1/src/tallerEmpresa/Empresa.java
+++ Archivos github/programacion 1/src/tallerEmpresa/Empresa.java
@@ -1,4 +1,98 @@
 package tallerEmpresa;
 
 public class Empresa {
+    private String nombre;
+    private String nit;
+    private Empleado[] listEmpleado;
+
+    public Empresa(String nombre, String nit) {
+        this.nombre = nombre;
+        this.nit = nit;
+        this.listEmpleado = new Empleado[50];
+    }
+    public Empleado[] buscarEmpleadoCargo(String cargoEmpleado) {
+        int cantEmpleadosCargo = 0;
+        for (int i = 0; i < listEmpleado.length; i++) {
+            if (listEmpleado[i].getCargo().equals(cargoEmpleado))
+                    cantEmpleadosCargo++;
+        }
+        Empleado[] listEmpleadosCargo = new Empleado[cantEmpleadosCargo];
+        int indice = 0;
+
+        for (int i = 0; i < listEmpleado.length; i++) {
+            if (listEmpleado[i].getCargo().equals(cargoEmpleado)){
+                listEmpleadosCargo[indice] = listEmpleado[i];
+                indice++;
+            }
+        }
+        return listEmpleadosCargo;
+    }
+
+    public boolean actulizarEmpleado(String idEmpleadoModificar, Empleado empleadomodificado) {
+        int posicionEmpleadoModificar = buscarEmpleadoID(idEmpleadoModificar);
+        if (posicionEmpleadoModificar == -1) {
+            listEmpleado[posicionEmpleadoModificar] = empleadomodificado;
+            return true;
+        }
+        return false;
+    }
+
+    public boolean crearEmpleado(Empleado newEmpleado) {
+        int espacioLibre = validarCupoEmpleado();
+
+        if(espacioLibre != -1 &&
+                buscarEmpleadoID(newEmpleado.getCedula()) == -1) {
+            listEmpleado[espacioLibre] = newEmpleado;
+            return true;
+        }
+        return false;
+    }
+
+    public boolean eliminarEmpleado(String idEmpleadoEliminar) {
+        int posicionEmpleadoEliminar = buscarEmpleadoID(idEmpleadoEliminar);
+
+        if (posicionEmpleadoEliminar != -1) {
+            listEmpleado[posicionEmpleadoEliminar] = null;
+            return true;
+        }
+        return false;
+    }
+
+    public int buscarEmpleadoID(String idEmpleadoBuscar) {
+        for (int i = 0; i < listEmpleado.length; i++) {
+            if (listEmpleado[i].getCedula().equals(idEmpleadoBuscar)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+    public int validarCupoEmpleado(){
+        for (int i = 0; i < listEmpleado.length; i++) {
+            if (listEmpleado[i] == null) {
+                return i;
+            }
+        }
+        return -1;
+    }
+    public Empleado{
+
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public String getNit() {
+        return nit;
+    }
+    public void setNit(String nit) {
+        this.nit = nit;
+    }
+    public Empleado[] getListEmpleado() {
+        return listEmpleado;
+    }
+    public void setListEmpleado(Empleado[] listEmpleado) {
+        this.listEmpleado = listEmpleado;
+    }
 }
--- Archivos github/programacion 1/src/tallerEmpresa/main.java
+++ Archivos github/programacion 1/src/tallerEmpresa/main.java
@@ -1,4 +1,9 @@
 package tallerEmpresa;
 
 public class main {
+    public static void main(String[] args) {
+
+        Empresa empresa = new Empresa();
+        boolean response = empresa.crearEmpleado(new Empleado())
+    }
 }
